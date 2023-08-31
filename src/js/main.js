46c7a84f0bacb270ed7d6036d5fc414bd3d04832
Polo <mphowitzer@gmail.com>
2023-08-31 15:37:28 -0600

    Revisión_del_master

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,3 @@
-// menu responsive
-const mainItems = document.querySelectorAll(
-    '.main-item'
-);
-
 
 // Botón flotante
 const trigger = document.querySelector("menu > .trigger");
@@ -12,7 +7,7 @@ trigger.addEventListener('click', (e) => {
 
 
 // menu responsive
-
+const mainItems = document.querySelectorAll('.main-item');
 mainItems.forEach((mainItem) => {
     mainItem.addEventListener('click', () => {
         mainItem.classList.toggle(
@@ -20,56 +15,3 @@ mainItems.forEach((mainItem) => {
         );
     })
 });
-
-// Cards
-
-const allLinks = document.querySelectorAll(".tabs a");
-const allTabs = document.querySelectorAll(".tab-content")
-const tabContentWrapper = document.querySelector(".tab-content-wrapper");
-
-const shiftTabs = (linkId) => {
-    allTabs.forEach((tab, i) => {
-
-        if (tab.id.includes(linkId)) {
-            allTabs.forEach((tabItem) => {
-                tabItem.style = `transform: translateY(-${i * 300}px);`;
-            });
-        }
-    });
-}
-
-allLinks.forEach((elem) => {
-    elem.addEventListener('click', function () {
-        const linkId = elem.id;
-        const hrefLinkClick = elem.href;
-
-        allLinks.forEach((link, i) => {
-            if (link.href == hrefLinkClick) {
-                link.classList.add("active");
-            } else {
-                link.classList.remove('active');
-            }
-        });
-
-        shiftTabs(linkId);
-    });
-});
-
-//? handle proper selection for initial load
-const currentHash = window.location.hash;
-
-let activeLink = document.querySelector(`.tabs a`);
-
-if (currentHash) {
-    const visibleHash = document.getElementById(
-        `${currentHash.replace('#', '')}`
-    );
-
-    if (visibleHash) {
-        activeLink = visibleHash;
-    }
-}
-
-activeLink.classList.toggle('active');
-
-shiftTabs(activeLink.id);
