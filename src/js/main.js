9a99c41f5522f27c76b76314e07520d511c30ef7
Polo <mphowitzer@gmail.com>
2023-12-05 14:16:13 -0600

    actualizado a 5-12-23
    Actualización del repo

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,43 @@ mainItems.forEach((mainItem) => {
         );
     })
 });
+
+// Card con carrusel
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    effect: 'fade',
+    loop: true,
+    speed: 300,
+    mousewheel: {
+        invert: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+// Carrusel para cards
+
+document.addEventListener('DOMContentLoaded', function () {
+    let imagenesQro = [
+        {img: '../src/img/fichas/Qro/img1.jpg'},
+        {img: '../src/img/fichas/Qro/img2.jpg'},
+        {img: '../src/img/fichas/Qro/img3.jpg'},
+        {img: '../src/img/fichas/Qro/img4.jpg'},
+        {img: '../src/img/fichas/Qro/img5.jpg'}
+    ]
+    let contador = 0;
+    const contenedor = document.querySelector('.slideshow');
+    const overlay = document.querySelector('.overlay');
+    const cardQro = document.querySelectorAll('.cardGaleria qro img');
+    const imgSlideShow = document.querySelector('.slideshow img')
+})
